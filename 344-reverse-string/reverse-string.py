922cd7b7fad9665a9f9f1d25f87aa4629448eaa1
Fatemeh Tavakoli <fm93tavakoli@gmail.com>
2024-10-03 11:53:41 -0400

    Time: 166 ms (72.50%) | Memory: 20.9 MB (37.57%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # start_pointer = s[0]
+        # end_pointer = s[-1]
+
+        s.reverse()
+
+        return s
+
+
+
+        
