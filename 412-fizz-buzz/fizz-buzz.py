69f6d9cebd9d004c41e61b2ec4e470d1e363d920
Fatemeh Tavakoli <fm93tavakoli@gmail.com>
2024-10-03 12:57:28 -0400

    Time: 33 ms (96.45%) | Memory: 17.6 MB (51.15%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -2,6 +2,9 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
       
+        # answer[i] == "FizzBuzz" wouldnt work becasue the index is out of bound... when start with answer[i] there is no ith index to be assigned new value
+
+
         for i in range(1,n+1): 
             if i%3==0 and i%5 ==0: 
                 answer.append("FizzBuzz")
