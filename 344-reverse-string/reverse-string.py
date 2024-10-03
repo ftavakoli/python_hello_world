a7839c1d2a7292c54b60cf64ec2711a953a3357e
Fatemeh Tavakoli <fm93tavakoli@gmail.com>
2024-10-03 12:23:57 -0400

    Time: 169 ms (56.86%) | Memory: 20.8 MB (37.57%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,27 +3,28 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+    
+#swaping the first and last 
+        s_len = len(s)-1
+        i = 0
+        while i < s_len-i: 
+            s[i], s[s_len-i] = s[s_len-i], s[i]
+            i += 1
+        return s   
+
+# swaping the first and last using temp variable
         # s_len = len(s)-1
         # i = 0
         # while i < s_len-i: 
-        #     s[i], s[s_len-i] = s[s_len-i], s[i]
+        #     temp_var = s[i]
+        #     s[i] = s[s_len-i] 
+        #     s[s_len-i] = temp_var
         #     i += 1
         # return s   
 
 
-        s_len = len(s)-1
-        i = 0
-        while i < s_len-i: 
-            
-            temp_var = s[i]
-            s[i] = s[s_len-i] 
-            s[s_len-i] = temp_var
-            i += 1
-        return s   
 
-
-
-        # using the reverse() built in method 
+# using the reverse() built in method 
         # s.reverse()
 
         # return s
