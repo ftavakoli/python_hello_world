2ad0f4007640796b7e26a60663a5c6e6f8cb087f
Fatemeh Tavakoli <fm93tavakoli@gmail.com>
2024-10-03 13:46:29 -0400

    Time: 108 ms (68.53%) | Memory: 19.1 MB (20.08%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,11 +1,28 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         
-        nums.sort()  # Sort the array
-        i = 1
-        while i < len(nums):
-            if nums[i-1] == nums[i]:
-                i += 2  # Skip over the pair
-            else:
-                return nums[i-1]  # Return the single number
-        return nums[-1]  # If the single number is at the end, return the last element
+        # to meet the requirments we have to go with XOR logic... 
+
+        single_num = 0 # n ^ 0 = n 
+        for num in nums: 
+            single_num = num ^ single_num
+        return single_num
+
+
+
+
+        # # time: O(nlog(n)), space: O(log(n))
+
+        # nums.sort()  # Sort the array
+        # i = 1
+        # while i < len(nums):
+        #     if nums[i-1] == nums[i]:
+        #         i += 2  # Skip over the pair
+        #     else:
+        #         return nums[i-1]  # Return the single number
+        # return nums[-1]  # If the single number is at the end, return the last element
+
+
+
+
+
