15bd19749fe3c43031f29bfc5065acc0bc3f6426
Fatemeh Tavakoli <fm93tavakoli@gmail.com>
2024-10-10 13:46:08 -0400

    Time: 41 ms (12.99%) | Memory: 16.3 MB (90.37%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.py
@@ -1,10 +1,10 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        appeard_dict = {}
+        appeard_dict = {} #maping the characters with their appearance count
 
         for char in s: 
             if char in appeard_dict: 
-                appeard_dict[char] += 1
+                #appeard_dict[char] += 1
                 return char
             else: 
                 appeard_dict[char] = 1
